b65aaa685d01e14cd7105dccb9c6fe53b458d9f8
youstinaatef1 <youstinaatef748@gmail.com>
2026-05-13 12:22:18 +0300

    phase1

--- controllers/validation/registerSchema.js
+++ controllers/validation/registerSchema.js
@@ -4,5 +4,6 @@ const registerSchema = Joi.object({
     userName: Joi.string().min(3).max(50).required(),
     email:Joi.string().email().required(),
     password:Joi.string().min(6).max(20).required(),
-    role:Joi.string().valid("admin", "user", "")
-})
+    role:Joi.string().valid("admin", "user").default("user")
+});
+module.exports = registerSchema;
