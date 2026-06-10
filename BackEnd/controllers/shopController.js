b71c641a570a7c93c9a51777e0c8442444b90c50
youstinaatef1 <youstinaatef748@gmail.com>
2026-06-10 10:23:31 +0300

    Phase 3

--- BackEnd/controllers/shopController.js
+++ BackEnd/controllers/shopController.js
@@ -2,4 +2,12 @@ const User = require("../models/User");
 const Shop = require("../models/Shop");
 const Product = require("../models/Product");
 const shopValidation = require("./validation/shopValidation");
-
+try {
+    if (!req.file) return res.status(400).json({ msg: "Please Add Your Logo" });
+    const{error, value} = shopValidation.validate(req.body, {
+            abortEarly: false,
+            stripUnknown: true
+        });
+} catch (error) {
+    
+}
